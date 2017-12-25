400e5008c95d29e490ec37f424c5524cb948a0d7
madhumathi1 <madhumathid28@gmail.com>
2017-12-24 22:49:37 -0800

    Create case.c

---
+++ case.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("Positive");
+}
+else if(a<0)
+{
+printf("negative");
+}
+else 
+{
+printf("Zero");
+}
+getch();
+}
